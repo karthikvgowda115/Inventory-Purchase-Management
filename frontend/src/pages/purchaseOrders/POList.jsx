13c7a1c16cb10cc3aeb322d0bd1288d3e3cb2589
Karthik C V <karthikvgowda115@gmail.com>
2026-08-11 17:02:44 +0530

    Update POList.jsx

--- frontend/src/pages/purchaseOrders/POList.jsx
+++ frontend/src/pages/purchaseOrders/POList.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axiosInstance from "../../api/axiosInstance";
-import axiosInstance from "../../services/axiosInstance";
+
 import {
   getPurchaseOrders,
   approvePurchaseOrder,
